76d1218a90da7a5404d35a39dca86cb14b30198b
Ilya Gorbachev <ilya-gorbachev01@yandex.ru>
2025-07-30 12:32:23 +0400

    add views, permissions, filters, DjangoFilterBackend

---
+++ api_yamdb/api/filters.py
@@ -0,0 +1,25 @@
+import django_filters
+
+from reviews.models import Title
+
+
+class TitleFilter(django_filters.FilterSet):
+    category = django_filters.CharFilter(
+        field_name='category__slug',
+        lookup_expr='iexact'  # точное совпадение без учёта регистра
+    )
+    genre = django_filters.CharFilter(
+        field_name='genre__slug',
+        lookup_expr='iexact'  # точное совпадение без учёта регистра
+    )
+    year = django_filters.NumberFilter(
+        field_name='year',
+        lookup_expr='exact'  # точное совпадение с учётом регистра
+    )
+    name = django_filters.CharFilter(
+        field_name='name',
+        lookup_expr='icontains'  # частичное совпадение без учёта регистра
+    )
+
+    class Meta:
+        model = Title
--- api_yamdb/api/v1/urls.py
+++ api_yamdb/api/v1/urls.py
@@ -2,9 +2,13 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from users.views import SignUpView, TokenView
+from api.views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 
 v1_router = DefaultRouter()
+v1_router.register('categories', CategoryViewSet)
+v1_router.register('genres', GenreViewSet)
+v1_router.register('titles', TitleViewSet)
 
 urlpatterns = [
     path('', include(v1_router.urls)),
