f2b4f0dcedd116d49d491a5421bdd954ff951937
Илья Горбачев <ilya-gorbachev01@yandex.ru>
2025-08-05 19:24:06 +0400

    register models in admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import YamdbUser
+from .models import YamdbUser, Category, Genre, Title, Review, Comment
 
 admin.site.empty_value_display = 'Не задано'
 
@@ -15,3 +15,35 @@ class YamdbUserAdmin(admin.ModelAdmin):
         'bio',
         'role'
     )
+
+
+@admin.register(Category, Genre)
+class NameSlugAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'category')
+    list_filter = ('year', 'category')
+    search_fields = ('name',)
+    filter_horizontal = ('genre',)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'score', 'pub_date')
+    list_filter = ('score', 'pub_date', 'title')
+    search_fields = ('text', 'author__username', 'title__name')
+    readonly_fields = ('pub_date',)
+    raw_id_fields = ('author', 'title')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'review', 'text', 'pub_date')
+    list_filter = ('pub_date', 'author')
+    search_fields = ('text', 'author__username', 'review__text')
+    readonly_fields = ('pub_date',)
+    raw_id_fields = ('author', 'review')
